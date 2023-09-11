f58d1df8d8973052b13f4fd1c18e195b379e078c
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-10 23:36:03 -0300

    Finalizando seção 1 (Graças a Deus)

--- fundamentos/01_Variaveis/VariaveisFuncoes.dart
+++ fundamentos/01_Variaveis/VariaveisFuncoes.dart
@@ -24,6 +24,10 @@ void main() {
   print(pi.isInfinite); // False
   print((pi / 0).isInfinite); // true
   print(12.gcd(16)); // 4
+  print("1".padLeft(2, "0"));
+  print(" ");
+  print(" ");
+  print(" ");
 
   String nome = "Nome";
   String nomecompleto = "\t Gabriel Estéfono";
@@ -39,4 +43,29 @@ void main() {
   print(nome.replaceAll("Nome", "Gabriel"));
   print(nome.replaceFirst("N", ""));
   print(nome.replaceRange(1, 3, "Gab"));
+  print(nomecompleto.trim().split(" "));
+  print(nome.contains("f"));
+  print(nome.contains("e"));
+  print(nome.indexOf("om"));
+  print(nome.length);
+  print(nome.endsWith("e"));
+  print(nome.compareTo("nome"));
+  print(nome.compareTo("Nome"));
+  print(nome.compareTo("ome"));
+  print(" ".isEmpty);
+  print("".isEmpty);
+  print(double.parse("12.55"));
+  print("1" is String);
+  print(int.parse("12"));
+  print(nome.lastIndexOf("e"));
+  String sopa = "Sopa de letrinhas";
+  int index = sopa.indexOf(" de ");
+  print(index);
+  print("A sopa é ${sopa.substring(index).trim()}");
+  print(" ");
+  print(" ");
+  print(" ");
+  num numero = 3.14;
+  print(numero is double);
+  print(numero is! double);
 }
