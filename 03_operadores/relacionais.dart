0fbb46beec04c3878e1d074e9dbbb8cbd0b2db44
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-12 22:03:17 -0300

    Operadores relacionais

---
+++ 03_operadores/relacionais.dart
@@ -0,0 +1,10 @@
+void main() {
+  print("02.1 - Operadores Relacionais");
+  print(">, >=, <, <=, == !=");
+
+  var nota = 69;
+  var resultado = nota >= 70;
+  print("Aluno aprovado: $resultado");
+  bool testelogico = (!false && true);
+  print(testelogico);
+}
