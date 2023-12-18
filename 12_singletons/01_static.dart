a354040267d5937d946763629a4b7bf8c67972b1
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-18 07:50:08 -0300

    Seção 12 - Singletons - Static

---
+++ 12_singletons/01_static.dart
@@ -0,0 +1,7 @@
+import 'PessoaStatic.dart';
+
+void main() {
+  print("12.1.0 - Singletons - Static");
+//   print(PessoaStatic.instancia.nome);
+  PessoaStatic.instancia.nome = "Gabriel";
+}
---
+++ 12_singletons/PessoaStatic.dart
@@ -0,0 +1,6 @@
+class PessoaStatic {
+  static final PessoaStatic instancia = new PessoaStatic._construtorNomeado();
+  late String nome;
+
+  PessoaStatic._construtorNomeado();
+}
