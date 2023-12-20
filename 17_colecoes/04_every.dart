38e50b28ba6cefbaaeadb7be5b90c652af301ee7
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-20 06:58:59 -0300

    Seção 17 - Every

---
+++ 17_colecoes/04_every.dart
@@ -0,0 +1,12 @@
+void main(){
+    listEvery();
+}
+
+listEvery(){
+    List<int> inteiros = [0,1,2,3,4,5];
+    bool resultado = inteiros.every((element) => element >= 0);
+    print(resultado);
+    List<String> textos = List.from(['Gabriel', 'Estéfono']);
+    resultado = textos.every((element) => element.contains("e"));
+    print(resultado);
+}
