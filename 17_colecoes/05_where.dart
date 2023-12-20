8f5de4f80ea1d96a1595feae924055de7d05afb1
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-20 07:17:22 -0300

    Seção 17 - Where

---
+++ 17_colecoes/05_where.dart
@@ -0,0 +1,12 @@
+void main(){
+    listaWhere();
+}
+
+listaWhere(){
+    List<int> idades = [36, 12, 10, 21, 17, 23];
+    print(idades.where((element) => element % 2 == 0));
+    print(idades.whereType<int>());
+    print(idades.singleWhere((element) => element < 12));
+    print(idades.lastWhere((element) => element > 12));
+    print(idades.firstWhere((element) => element > 12));
+}
