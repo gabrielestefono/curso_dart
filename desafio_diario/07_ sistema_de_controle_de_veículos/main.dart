ffd53e5e090963b41041a6619d82a54e917914a0
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-19 23:32:01 -0300

    Desafio Diário - 7º Dia - Início.

---
+++ desafio_diario/07_ sistema_de_controle_de_veículos/main.dart
@@ -0,0 +1,44 @@
+/*
+Classes Básicas:
+Veiculo: Classe base para todos os tipos de veículos.
+Carro, Moto, Caminhao: Classes que herdam de Veiculo.
+
+Características da Classe Veiculo:
+Atributos: marca, modelo, ano de fabricação.
+Construtor para inicializar os atributos.
+Encapsulamento: Use getters e setters.
+Método virtual exibirInfo() para exibir informações do veículo.
+
+Sobrescrita de Método:
+Sobrescreva o método exibirInfo() nas classes derivadas (Carro, Moto, Caminhao) para exibir informações específicas de cada tipo de veículo.
+
+Interface:
+Crie uma interface Manutencao com um método realizarManutencao().
+
+Implementação da Interface:
+Implemente a interface Manutencao em pelo menos uma das classes derivadas, como Carro.
+
+Mixin:
+Crie um mixin RecursosAdicionais com métodos ou propriedades adicionais, como ativarGPS().
+
+Uso de Mixin:
+Aplique o mixin RecursosAdicionais em algumas das classes de veículos.
+
+Singleton:
+Implemente uma classe GerenciadorDeVeiculos como um singleton, responsável por gerenciar uma lista de veículos.
+
+Funcionalidades do GerenciadorDeVeiculos:
+Métodos para adicionar e remover veículos.
+Método para listar todos os veículos.
+Método para buscar veículos por marca ou modelo.
+
+Operadores, Condicionais, Loops, Arrays:
+Utilize esses elementos para implementar as funcionalidades da classe GerenciadorDeVeiculos.
+
+Tarefa:
+Implemente o sistema conforme os requisitos acima. Teste as funcionalidades criando várias instâncias de diferentes tipos de veículos e utilizando os métodos do GerenciadorDeVeiculos, além de testar a sobrescrita de métodos, a implementação da interface e o uso do mixin.
+*/
+
+void main(){
+    print("Só pra falar que eu comecei o desafio.");
+}
