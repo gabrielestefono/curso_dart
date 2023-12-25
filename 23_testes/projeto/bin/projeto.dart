d530c4c6bf5448546a6084084c71b73719242214
gabrielestefono <gabrielestefono@hotmail.com>
2023-12-25 08:32:56 -0300

    Seção 23 - Testes

---
+++ 23_testes/projeto/bin/projeto.dart
@@ -0,0 +1,7 @@
+import 'package:projeto/projeto.dart' as projeto;
+
+void main(List<String> arguments) {
+    print("23.0.0 - Testes unitários");
+    String texto = "adas";
+    print('Hello world: ${projeto.calculate()}!');
+}
