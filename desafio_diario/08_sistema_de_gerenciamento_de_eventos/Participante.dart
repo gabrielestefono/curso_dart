f95b44bd9b29a2f7ab27824e03c2ea0011551afe
gabrielestefono <gabrielestefono@hotmail.com>
2024-01-02 20:12:27 -0300

    Desafio Diário - 8º Dia - UPDATE - Mudar Nome Partifipante

--- desafio_diario/08_sistema_de_gerenciamento_de_eventos/Participante.dart
+++ desafio_diario/08_sistema_de_gerenciamento_de_eventos/Participante.dart
@@ -1,6 +1,18 @@
+import 'dart:io';
+
 class Participante{
     String nome;
     String email;
+
+    void editarNome(){
+        print("Digite o novo nome: ");
+        String? nome = stdin.readLineSync();
+        if(nome != null && nome != ''){
+            this.nome = nome;
+        }else{
+            print("Ocorreu um erro, por favor, tente novamente!");
+        }
+    }
 
     Participante(this.nome, this.email);
 }
